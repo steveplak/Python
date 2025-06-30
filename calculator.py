cdec045f651d237cd09f11ab4f8593937917e62b
steveplak <stplakantonakis@gmail.com>
2025-06-30 22:30:31 +0100

    changed

---
+++ calculator.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk() 
+    
+    DiceGameLayout(root)
+    root.mainloop()
+
+
+
+def DiceGameLayout(root):
+     
+    label_1 = tk.Label(root,text="Dice Game")
+    label_1.pack()
+
+    label_2 = tk.Label(root,text="Computer")
+    label_2.pack()
+
+    
+
+    Text_Box_1 = tk.Entry(root, justify= "center")
+    Text_Box_1.insert(0,"Computer")
+    Text_Box_1.pack()
+
+main()
